761cb980769b022ad3af78c9238bcf23e20cff6e
alex <forn.sky@yandex.ru>
2020-11-30 15:33:41 +0300

    init

---
+++ src/main/java/state/machine/StateMachineSpringBootApplication.java
@@ -0,0 +1,13 @@
+package state.machine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StateMachineSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StateMachineSpringBootApplication.class, args);
+    }
+
+}
